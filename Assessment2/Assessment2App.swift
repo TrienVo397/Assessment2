dff8b6a72c71be5bd55b2e6c723d700b296dec0f
Trien Vo <s3907397@rmit.edu.vn>
2023-08-29 13:13:32 +0800

    Initial Commit

---
+++ Assessment2/Assessment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assessment2App.swift
+//  Assessment2
+//
+//  Created by user236506 on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Assessment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
